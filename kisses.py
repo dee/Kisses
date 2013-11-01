c88aefc1d92e33631470445448423968c3c87ede
Alex <priamid@gmail.com>
2013-11-01 12:26:19 +0100

    Implemented Hoogle search command. Cleanups. Fixes. Swutched to monospace font in dialogs

--- kisses.py
+++ kisses.py
@@ -29,7 +29,11 @@ class GettypeCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		fileName = self.view.file_name()
 		symbol = getSymbol(self.view)
-		print("Checking symbol " + symbol +" in filename : "+fileName)
+		if not symbol:
+			sublime.status_message('GetType: empty selection or no symbol under cursor.')
+			return
+
+		#print("Checking symbol " + symbol +" in filename : "+fileName)
 		h = Kisses.kisseslib.Hugs()
 		response = h.runHugs(fileName, ':t '+symbol+'\n')
 		result = h.processResponse(response, Kisses.kisseslib.TypeParser(symbol))
@@ -39,33 +43,62 @@ class GettypeCommand(sublime_plugin.TextCommand):
 				# todo: show errors
 				pass
 			if type(item) is Kisses.kisseslib.HugsMessage:
-				# todo: show results
+				# show results
 				if not item.message in getTypeHistory:
 					getTypeHistory.append(item.message)
 					if len(getTypeHistory) > getTypeHistoryLength:
 						getTypeHistory.pop()
 				# show popup
-				sublime.active_window().show_quick_panel(getTypeHistory, on_select=None)
+				sublime.active_window().show_quick_panel(getTypeHistory, 
+					on_select=None,
+					flags=sublime.MONOSPACE_FONT)
 				gotMessage = True
 				pass
 		if not gotMessage:
 			# show default message, e.g. "symbol not found"
 			sublime.status_message('GetType: no info on '+symbol)
-			pass
+			
 
 
 class GettypehistoryCommand(sublime_plugin.TextCommand):
 	""" Shows a box with previously requested type info """
 
 	def run(self, edit):
-		print("Showing a type search history.")
-		print(getTypeHistory)
-		sublime.active_window().show_quick_panel(getTypeHistory, on_select=None)
+		#print("Showing a type search history.")
+		#print(getTypeHistory)
+		sublime.active_window().show_quick_panel(getTypeHistory, 
+			on_select=None,
+			flags=sublime.MONOSPACE_FONT)
 
 
 class HooglesearchCommand(sublime_plugin.TextCommand):
 	""" asks hoogle about text at cursor, shows results in separate window """
 
+	currentResults = []
+
 	def run(self, edit):
-		print("Searching in Hoogle")
-		pass
+		symbol = getSymbol(self.view)
+		if not symbol:
+			sublime.status_message('Hoogle: empty selection or no symbol under cursor.')
+			return
+		h = Kisses.kisseslib.Hoogle()
+		results = h.getSuggestions(symbol)
+		# trim docstrings
+		self.currentResults = []
+		maxLen = 200
+		for result in results:
+			docstring = result[2]
+			if len(docstring) >= maxLen:
+				docstring = docstring[0:maxLen-3]
+				docstring += "..."
+				result[2] = docstring
+			self.currentResults.append(result)
+
+		# todo: on select open new window with results
+		sublime.active_window().show_quick_panel(self.currentResults, 
+			on_select=self.selectEntry, 
+			flags=sublime.MONOSPACE_FONT)
+
+	def selectEntry(self, index):
+		selection = self.currentResults[index]
+		# todo: continue
